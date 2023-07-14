e7cbde3119a5a93eb72f23bdaa11b218d674af1c
ReVive31715 <128776778+ReVive31715@users.noreply.github.com>
2023-07-15 02:29:18 +0900

    v0.3

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 import tkinter.messagebox as msgbox
+import pandas as pd
 from math import *
 from tkinter import *
 
-#---------------------
+#----------메뉴-----------
+
+def credit():
+    creditwin = Toplevel(root)
+    creditwin.geometry('320x200+100+100')
+    creditname = Label(creditwin, text = '천능 계산기', font = 30).pack()
+    creditmans = Message(creditwin, width = 300, text = '만든이 : 신동흠, 이윤민, 노광민, 김민기\n태성고등학교 HACKATON\nPython 3 사용').pack()
+    
 
 def quitConfirm():
     response = msgbox.askyesno('종료', '종료하시겠습니까?')
@@ -10,10 +18,10 @@ def quitConfirm():
         close()
     
 def close():
-    cal.quit()
-    cal.destroy()
+    root.quit()
+    root.destroy()
 
-#---------------------
+#----------연산-----------
 
 def btn_num(number):
     current = result.get()
@@ -25,8 +33,11 @@ def btn_num(number):
 
 def btn_dot():
     current = result.get()
-    result.delete(0,)
-    result.insert(0, str(current) + '.')
+    if current.find('.') != -1:
+        pass
+    else:
+        result.delete(0, END)
+        result.insert(0, str(current) + '.')
 
 def btn_clear():
     result.delete(0, END)
@@ -44,18 +55,18 @@ def btn_equal():
     result.delete(0, END)
     try:
         if math == '더하기':
-            result.insert(0, f_num + float(second_number))
+            result.insert(0, round(f_num + float(second_number), 7))
         if math == '빼기':
-            result.insert(0, f_num - float(second_number))
+            result.insert(0, round(f_num - float(second_number), 7))
         if math == '곱하기':
-            result.insert(0, f_num * float(second_number))
+            result.insert(0, round(f_num * float(second_number), 7))
         if math == '나누기':
             if second_number == '0':
                 result.insert(0, 'ERROR')
             else:
-                result.insert(0, f_num / float(second_number))
+                result.insert(0, round(f_num / float(second_number), 7))
         if math == '제곱':
-            result.insert(0, f_num ** float(second_number))
+            result.insert(0, round(f_num ** float(second_number), 7))
     except:
         pass
  
@@ -86,30 +97,42 @@ def btn_divide():
 def btn_sin():
     inputnumber = result.get()
     result.delete(0, END)
-    result.insert(0, round(sin(radians(int(inputnumber))), 5))
+    if raddeg.get() == 0:
+        result.insert(0, round(sin(radians(float(inputnumber))), 7))
+    elif raddeg.get() == 1:
+        result.insert(0, round(sin(float(inputnumber)), 7))
 
 def btn_cos():
     inputnumber = result.get()
     result.delete(0, END)
-    result.insert(0, round(cos(radians(int(inputnumber))), 5))
+    if raddeg.get() == 0:
+        result.insert(0, round(cos(radians(float(inputnumber))), 7))
+    elif raddeg.get() == 1:
+        result.insert(0, round(cos(float(inputnumber)), 7))
 
 def btn_tan():
     inputnumber = result.get()
     result.delete(0, END)
-    if (int(inputnumber) % 90 == 0) and ((int(inputnumber) // 90) % 2 != 0):
-        result.delete(0, END)
-        result.insert(0, 'ERROR')
-    else:
-        result.insert(0, round(tan(radians(int(inputnumber))), 5))
+    if raddeg.get() == 0:
+        if (int(inputnumber) % 90 == 0) and ((int(inputnumber) // 90) % 2 == 1):
+            result.insert(0, 'ERROR')
+        else:
+            result.insert(0, round(tan(radians(float(inputnumber))), 7))
+
+    elif raddeg.get() == 1:
+        if (round(float(inputnumber) % 1.5707963) == 0) and (round(float(inputnumber) // 1.5707963 % 2) == 1):
+            result.insert(0, 'ERROR')
+        else:
+            result.insert(0, round(tan(float(inputnumber)), 7))
 
 def btn_pi():
     result.delete(0, END)
-    result.insert(0, round(pi, 5))
+    result.insert(0, round(pi, 7))
 
 def btn_sqrt():
     inputnumber = result.get()
     result.delete(0, END)
-    result.insert(0, round(sqrt(float(inputnumber)), 5))
+    result.insert(0, round(sqrt(float(inputnumber)), 7))
 
 def btn_power():
     first_number = result.get()
@@ -122,61 +145,66 @@ def btn_power():
 def btn_log():
     inputnumber = result.get()
     result.delete(0, END)
-    result.insert(0, round(log10(float(inputnumber)), 5))
-#---------------------
+    result.insert(0, round(log10(float(inputnumber)), 7))
+#----------GUI-----------
 
-cal = Tk() #GUI 생성
-cal.title('Calculator') #GUI 제목
-cal.geometry('600x500')
-cal.resizable(False, False) #GUI 크기 조정 불가
+pd.options.display.float_format = '{:.7f}'.format
+root = Tk() #GUI 생성
+root.title('Calculator') #GUI 제목
+root.geometry('600x550')
+root.resizable(False, False) #GUI 크기 조정 불가
 
-menubar = Menu(cal)
+menubar = Menu(root)
 menu_1 = Menu(menubar, tearoff = 0)
-menu_1.add_command(label = '천능 계산기')
-menu_1.add_command(label = '물리량 계산기')
+menu_1.add_command(label = '만든이', command = credit)
 menu_1.add_separator()
 menu_1.add_command(label = '닫기', command = quitConfirm)
 menubar.add_cascade(label = '메뉴', menu = menu_1)
 
 menu_2 = Menu(menubar, )
-cal.config(menu = menubar)
+root.config(menu = menubar)
 
-label = Label(cal, text = '천능 계산기', font=30, pady = 10)
+label = Label(root, text = '만능은 아니구 천능 계산기', font=30, pady = 10)
 label.pack()
 
-result = Entry(cal, text = '0', font = 30, justify = 'right', background='white')
+result = Entry(root, text = '0', font = 30, justify = 'right', background='white')
 result.place(x = 50, y = 60, width = 500, height = 30)
 
-b1 = Button(cal, text = '1', font = 30, command=lambda : btn_num(1)).place(x = 200, y = 100, width = 100, height = 100)
-b2 = Button(cal, text = '2', font = 30, command=lambda : btn_num(2)).place(x = 300, y = 100, width = 100, height = 100)
-b3 = Button(cal, text = '3', font = 30, command=lambda : btn_num(3)).place(x = 400, y = 100, width = 100, height = 100)
-b4 = Button(cal, text = '4', font = 30, command=lambda : btn_num(4)).place(x = 200, y = 200, width = 100, height = 100)
-b5 = Button(cal, text = '5', font = 30, command=lambda : btn_num(5)).place(x = 300, y = 200, width = 100, height = 100)
-b6 = Button(cal, text = '6', font = 30, command=lambda : btn_num(6)).place(x = 400, y = 200, width = 100, height = 100)
-b7 = Button(cal, text = '7', font = 30, command=lambda : btn_num(7)).place(x = 200, y = 300, width = 100, height = 100)
-b8 = Button(cal, text = '8', font = 30, command=lambda : btn_num(8)).place(x = 300, y = 300, width = 100, height = 100)
-b9 = Button(cal, text = '9', font = 30, command=lambda : btn_num(9)).place(x = 400, y = 300, width = 100, height = 100)
-b0 = Button(cal, text = '0', font = 30, command=lambda : btn_num(0)).place(x = 300, y = 400, width = 100, height = 100)
+raddeg = IntVar()
+rad2deg = Radiobutton(root, text = 'Degree', value = 0, variable=raddeg).place(x = 50, y = 100)
+deg2rad = Radiobutton(root, text = 'Radian', value = 1, variable=raddeg).place(x = 150, y = 100)
 
-bclear = Button(cal, text = 'Clear', font = 30,background = 'orange', command = btn_clear).place(x = 100, y = 400, width = 100, height = 100)
-bequal = Button(cal, text = '=', font = 30, background = 'orange', command = btn_equal).place(x = 400, y = 400, width = 100, height = 100)
-bsum = Button(cal, text = '+', font = 30, command = btn_add).place(x = 500, y = 100, width = 100, height = 100)
-bsub = Button(cal, text = '-', font = 30, command = b_subtract).place(x = 500, y = 200, width = 100, height = 100)
-bmult = Button(cal, text = '*', font = 30, command = btn_multiply).place(x = 500, y = 300, width = 100, height = 100)
-bdiv = Button(cal, text = '/', font = 30, command = btn_divide).place(x = 500, y = 400, width = 100, height = 100)
 
-bsqrt = Button(cal, text = '√', font = 30, command = btn_sqrt).place(x = 100, y = 100, width = 100, height = 100)
-bpower = Button(cal, text = '^', font = 30, command = btn_power).place(x = 100, y = 200, width = 100, height = 100)
-blog = Button(cal, text = 'log', font = 30, command = btn_log).place(x = 100, y = 300, width = 100, height = 100)
-ddot = Button(cal, text = '.', font = 30, command = btn_dot).place(x = 200, y = 400, width = 100, height = 100)
+b1 = Button(root, text = '1', font = 30, cursor = 'hand2', relief = 'groove', command=lambda : btn_num(1)).place(x = 200, y = 150, width = 100, height = 100)
+b2 = Button(root, text = '2', font = 30, cursor = 'hand2', relief = 'groove', command=lambda : btn_num(2)).place(x = 300, y = 150, width = 100, height = 100)
+b3 = Button(root, text = '3', font = 30, cursor = 'hand2', relief = 'groove', command=lambda : btn_num(3)).place(x = 400, y = 150, width = 100, height = 100)
+b4 = Button(root, text = '4', font = 30, cursor = 'hand2', relief = 'groove', command=lambda : btn_num(4)).place(x = 200, y = 250, width = 100, height = 100)
+b5 = Button(root, text = '5', font = 30, cursor = 'hand2', relief = 'groove', command=lambda : btn_num(5)).place(x = 300, y = 250, width = 100, height = 100)
+b6 = Button(root, text = '6', font = 30, cursor = 'hand2', relief = 'groove', command=lambda : btn_num(6)).place(x = 400, y = 250, width = 100, height = 100)
+b7 = Button(root, text = '7', font = 30, cursor = 'hand2', relief = 'groove', command=lambda : btn_num(7)).place(x = 200, y = 350, width = 100, height = 100)
+b8 = Button(root, text = '8', font = 30, cursor = 'hand2', relief = 'groove', command=lambda : btn_num(8)).place(x = 300, y = 350, width = 100, height = 100)
+b9 = Button(root, text = '9', font = 30, cursor = 'hand2', relief = 'groove', command=lambda : btn_num(9)).place(x = 400, y = 350, width = 100, height = 100)
+b0 = Button(root, text = '0', font = 30, cursor = 'hand2', relief = 'groove', command=lambda : btn_num(0)).place(x = 200, y = 450, width = 100, height = 100)
 
-bsin = Button(cal, text = 'sin', font = 30, command = btn_sin).place(x = 0, y = 100, width = 100, height = 100)
-bcos = Button(cal, text = 'cos', font = 30, command = btn_cos).place(x = 0, y = 200, width = 100, height = 100)
-btan = Button(cal, text = 'tan', font = 30, command = btn_tan).place(x = 0, y = 300, width = 100, height = 100)
-bpi = Button(cal, text = 'π', font = 30, command = btn_pi).place(x = 0, y = 400, width = 100, height = 100)
+bclear = Button(root, text = 'Clear', font = 30, background = 'orange', cursor = 'hand2', relief = 'groove', command = btn_clear).place(x = 100, y = 450, width = 100, height = 100)
+bequal = Button(root, text = '=', font = 30, background = 'orange', cursor = 'hand2', relief = 'groove', command = btn_equal).place(x = 400, y = 450, width = 100, height = 100)
+bsum = Button(root, text = '+', font = 30, cursor = 'hand2', relief = 'groove', command = btn_add).place(x = 500, y = 150, width = 100, height = 100)
+bsub = Button(root, text = '-', font = 30, cursor = 'hand2', relief = 'groove', command = b_subtract).place(x = 500, y = 250, width = 100, height = 100)
+bmult = Button(root, text = '*', font = 30, cursor = 'hand2', relief = 'groove', command = btn_multiply).place(x = 500, y = 350, width = 100, height = 100)
+bdiv = Button(root, text = '/', font = 30, cursor = 'hand2', relief = 'groove', command = btn_divide).place(x = 500, y = 450, width = 100, height = 100)
 
-#------------------------
+bsqrt = Button(root, text = '√', font = 30, cursor = 'hand2', relief = 'groove', command = btn_sqrt).place(x = 100, y = 150, width = 100, height = 100)
+bpower = Button(root, text = '^', font = 30, cursor = 'hand2', relief = 'groove', command = btn_power).place(x = 100, y = 250, width = 100, height = 100)
+blog = Button(root, text = 'log', font = 30, cursor = 'hand2', relief = 'groove', command = btn_log).place(x = 100, y = 350, width = 100, height = 100)
+ddot = Button(root, text = '.', font = 30, cursor = 'hand2', relief = 'groove', command = btn_dot).place(x = 300, y = 450, width = 100, height = 100)
 
-cal.mainloop()
+bsin = Button(root, text = 'sin', font = 30, cursor = 'hand2', relief = 'groove', command = btn_sin).place(x = 0, y = 150, width = 100, height = 100)
+bcos = Button(root, text = 'cos', font = 30, cursor = 'hand2', relief = 'groove', command = btn_cos).place(x = 0, y = 250, width = 100, height = 100)
+btan = Button(root, text = 'tan', font = 30, cursor = 'hand2', relief = 'groove', command = btn_tan).place(x = 0, y = 350, width = 100, height = 100)
+bpi = Button(root, text = 'π', font = 30, cursor = 'hand2', relief = 'groove', command = btn_pi).place(x = 0, y = 450, width = 100, height = 100)
+
+#-----------실행과 종료 보고-------------
+
+root.mainloop()
 
 print('Programm Terminated')
